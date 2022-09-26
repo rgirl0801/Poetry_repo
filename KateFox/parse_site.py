ca1622936632450f4fae37ac06d85694a63dea92
Kate <r_girl@inbox.ru>
2022-09-26 09:27:16 -0400

    add to folder

--- KateFox/parse_site.py
+++ KateFox/parse_site.py
@@ -3,14 +3,14 @@ from bs4 import BeautifulSoup
 
 
 def count_selenium():
-    url = 'https://www.selenium.dev/'
+    url = "https://www.selenium.dev/"
     resp = requests.get(url)
 
     if resp.status_code == 200:
         print("Successfully opened the web page")
-        soup = BeautifulSoup(resp.text, 'html.parser')
+        soup = BeautifulSoup(resp.text, "html.parser")
         all_text = soup.get_text()
-        n = all_text.count('Selenium')
+        n = all_text.count("Selenium")
         print(f'{n} times the word "Selenium" occurs')
     else:
         print("Error")

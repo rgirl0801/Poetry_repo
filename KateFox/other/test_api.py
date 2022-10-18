cb88d5f8f66d584c231b7d034b42851bba3d2cda
KateFox <52407432+rgirl0801@users.noreply.github.com>
2022-10-17 20:27:18 -0400

    Delete KateFox directory

--- KateFox/other/test_api.py
+++
@@ -1,7 +0,0 @@
-
-from KateFox.other.api import check_lesson_name
-
-def test_check_lesson_name():
-    # lesson_url = 'https://stepik.org/lesson/223529'
-    lesson_id = 223529
-    check_lesson_name(lesson_id=lesson_id, expected_name="wrong_name")
